ec8850435790199e36c8eac1f0a78815701cfc5e
GenifeUA <artemzheglov10@gmail.com>
2024-04-14 16:29:23 +0200

    start.bat update

--- start.bat
+++ start.bat
@@ -1,8 +1,27 @@
 @echo off
+setlocal
 
-IF NOT EXIST .venv (
-    python -m venv .venv
+REM Define the path to the virtual environment
+set VENV=venv
+
+REM Check and create virtual environment if it does not exist
+if not exist "%VENV%\Scripts\python.exe" (
+    python -m venv %VENV%
+    echo Created virtual environment in %VENV%
+) else (
+    echo Virtual environment exists
 )
 
-.venv\Scripts\python -m pip install -r requirements.txt
-.venv\Scripts\python -m app.main
+REM Activate the virtual environment
+call %VENV%\Scripts\activate
+
+REM Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+REM Run the app.main module
+python -m app.main
+
+REM Deactivate the virtual environment
+deactivate
+
+endlocal
